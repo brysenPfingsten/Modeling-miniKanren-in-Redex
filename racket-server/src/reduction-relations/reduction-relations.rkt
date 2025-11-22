c6553ce0443a5af371a5b83b7434d60f9650fb88
Jason Hemann <jason.hemann@gmail.com>
2025-11-22 18:53:24 -0500

    Without domains we need syntax forms

--- racket-server/src/reduction-relations/reduction-relations.rkt
+++ racket-server/src/reduction-relations/reduction-relations.rkt
@@ -14,24 +14,9 @@
 (define (step-once prog)
   (apply-reduction-relation/tag-with-names -->cfg/whole (term ,prog)))
 
-(define -->test
-  (reduction-relation
-    Core
-    #:domain c
-    #:codomain (u ...)
-
-    [--> (u u_1 ...)
-         (u:2 ...)
-         (fresh ((u:2 ...) (u_1 ...)))
-         "test rule"]))
-
-
-
 (define -->cfg/whole
   (reduction-relation
     Core
-    #:domain (side-condition (name prog config) (judgment-holds (wf-program? prog)))
-    #:codomain (side-condition (name prog-out config) (judgment-holds (wf-program? prog-out)))
 
     [--> (Γ (σ ...) (⊤ σ_new))
          (Γ (σ ... σ_new) (empty-tree))]))
@@ -40,10 +25,8 @@
 (define -->e
   (reduction-relation
     Core
-    #:domain s
-    #:codomain s
 
-    [--> ((g_1 ∧ g_2 _) σ)
+    [--> ((g_1 ∧ g_2 tag) σ)
          ((g_1 σ) × g_2)
          "Distribute State Over Conjunction"]
 
@@ -51,13 +34,13 @@
          (g σ)
          "Bring Success State To Second Conjunct"]
 
-    [--> ((empty-tree) × _)
+    [--> ((empty-tree) × tag)
          (empty-tree)
          "Prune Failed Conjuncts"]
 
-    [--> ((∃ (x ...) g _) (state sub c trail o))
-         ((substitute g (u ...)) (state sub (u ... ,@(term c))  trail o))
-         (fresh ((u ...) (x ...)))
+    [--> ((∃ (x ...) g tag) (state sub c trail tag_1))
+         ((substitute g (u: x) ...) (state sub (u: ... ,@(term c)) trail tag_1))
+         (fresh ((u: ...) (x ...)))
          "Substitute Fresh Variables"]
 
     [--> ((t_1 =? t_2 tag) (state sub c ((t_3 =? t_4 tag_1) ...) tag_2))
@@ -65,7 +48,7 @@
          (where sub_1 (unify (walk t_1 sub) (walk t_2 sub) sub))
          "Unification Succeeds"]
 
-    [--> ((t_1 =? t_2 _) (state sub _ _ _))
+    [--> ((t_1 =? t_2 tag) (state sub c trail tag_2))
          (empty-tree)
          (where #f (unify (walk t_1 sub) (walk t_2 sub) sub))
           "Unification Fails"]
@@ -90,10 +73,10 @@
    (apply-reduction-relation -->*e trivial-conjunction-tree)
    (list (term (((succeed) (state () () () (label "cat"))) × (succeed)))))
 
-  (define (red-tree-closed-under-s? st)
-    (match-let ([(list st^) (apply-reduction-relation -->*e st)])
-      (redex-match? Core s st^)))
+  (define (-->*e-closed? st)
+    (let ([st* (apply-reduction-relation -->*e st)])
+      (andmap (lambda (st^) (redex-match? Core s st^)) st*)))
 
-  (check-reduction-relation -->*e red-tree-closed-under-s?)
+  (check-reduction-relation -->*e -->*e-closed?)
 
   )
